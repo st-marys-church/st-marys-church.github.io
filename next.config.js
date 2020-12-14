bc567e83a16bbdb92e280226b3e391e65e0363c2
Joseph Fonseka <hevayo@gmail.com>
2020-12-14 12:42:54 +0530

    Update base path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 const optimizedImages = require("next-optimized-images");
 
-module.exports = optimizedImages;
-module.exports.basePath = "/st-marys-colombo4"
+
+module.exports = {
+    basePath: "/st-marys-colombo4",
+    ...optimizedImages
+}
