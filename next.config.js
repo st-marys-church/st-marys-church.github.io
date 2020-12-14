11a2250f713d3f11aba220e1b1d9389c7a5f14ea
Joseph Fonseka <hevayo@gmail.com>
2020-12-14 12:51:02 +0530

    Update base path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-const optimizedImages = require("next-optimized-images");
+const isGhPages = process.env.NODE_ENV === 'gh-pages'
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
+module.exports = withPlugins([
+    [optimizedImages, {
 
-module.exports = {
-    basePath: "/st-marys-colombo4",
-    ...optimizedImages
-}
+    }],
+    {
+        basePath: isGhPages ? '/st-marys-colombo4' : '',
+        exportTrailingSlash: true
+    }
+]);
