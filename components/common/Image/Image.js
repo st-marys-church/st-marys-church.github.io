20d8c27008c13e08f875fdc4637f7302c42ec758
Joseph Fonseka <hevayo@gmail.com>
2020-12-22 08:31:03 +0530

    Fix image Urls

--- components/common/Image/Image.js
+++ components/common/Image/Image.js
@@ -4,12 +4,12 @@ import { getImageUrl } from "../../../utils/helpers";
 export function Image({ alt, src, previewSrc, webpSrc, className }) {
   return (
     <picture className={className}>
-      <source type="image/webp" data-srcset={webpSrc} />
-      <source type="image/png" data-srcset={src} />
+      <source type="image/webp" data-srcset={getImageUrl(webpSrc)} />
+      <source type="image/png" data-srcset={getImageUrl(src)} />
       <img
         className={`lazyload blur ${className} img-content`}
         alt={alt}
-        src={webpSrc}
+        src={getImageUrl(webpSrc)}
       />
     </picture>
   );
