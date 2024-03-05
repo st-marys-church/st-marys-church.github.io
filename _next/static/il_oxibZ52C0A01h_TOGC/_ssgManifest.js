ce578d9b652806dc58994155086a3bcb6351e68e
hevayo <hevayo@users.noreply.github.com>
2024-03-05 08:05:22 +0000

    deploy: d0fd9bceee3721b7beba03134af2bf9c86866c13

--- _next/static/il_oxibZ52C0A01h_TOGC/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002F","\u002Fcontact","\u002Finformation","\u002Fparish-associations","\u002Fparish-projects","\u002Fabout","\u002Fpost\u002F[slug]","\u002Fassociations\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
